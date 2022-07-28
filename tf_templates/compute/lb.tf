0fe56b23627490ec6f7766b5273d5ec956cf89c2
Your Name <thusitha.m@informaticsint.com>
2022-07-28 11:26:33 +0530

    Fixed syntax errors

--- tf_templates/compute/lb.tf
+++ tf_templates/compute/lb.tf
@@ -30,6 +30,6 @@ resource "aws_lb_target_group" "app-tg" {
 # ASG attachment to target group
 resource "aws_autoscaling_attachment" "as-attachment" {
   autoscaling_group_name = aws_autoscaling_group.app-asg.id
-  alb_target_group_arn   = aws_lb_target_group.app-tg.arn
+  lb_target_group_arn    = aws_lb_target_group.app-tg.arn
 }
 
