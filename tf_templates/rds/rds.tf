5fb72cdb82cce63885e4b4d4ac72bb9f821ad50f
Your Name <thusitha.m@informaticsint.com>
2022-07-28 15:49:29 +0530

    Fixed logic errors for db creation from aws

--- tf_templates/rds/rds.tf
+++ tf_templates/rds/rds.tf
@@ -1,18 +1,22 @@
 # DB master instance
 resource "aws_db_instance" "db" {
   # db_name         = var.db_name
-  allocated_storage      = 5
-  engine                 = var.db_engine
-  engine_version         = var.db_engine_version
-  instance_class         = var.db_instace_class
-  identifier             = var.db_identifier
-  username               = var.username
-  db_subnet_group_name   = aws_db_subnet_group.db-subnet-group.name
-  password               = random_string.postgres-db-password.result
-  vpc_security_group_ids = [var.db_security_group_id]
-  skip_final_snapshot    = true
-  publicly_accessible    = false
-  multi_az               = false
+  allocated_storage        = 5
+  engine                   = var.db_engine
+  engine_version           = var.db_engine_version
+  instance_class           = var.db_instace_class
+  identifier               = var.db_identifier
+  username                 = var.username
+  db_subnet_group_name     = aws_db_subnet_group.db-subnet-group.name
+  password                 = random_string.postgres-db-password.result
+  backup_retention_period  = 1
+  backup_window            = "09:46-10:16"
+  maintenance_window       = "Mon:00:00-Mon:03:00"
+  delete_automated_backups = true
+  vpc_security_group_ids   = [var.db_security_group_id]
+  skip_final_snapshot      = true
+  publicly_accessible      = false
+  multi_az                 = false
 
   tags = {
     "Name" = "tech-challenge-db-master"
@@ -22,7 +26,7 @@ resource "aws_db_instance" "db" {
 # DB read replicas
 resource "aws_db_instance" "db-read-replica" {
   # db_name           = var.db_name
-  count                  = var.number_of_read_replicas 
+  count                  = var.number_of_read_replicas
   replicate_source_db    = aws_db_instance.db.identifier
   identifier             = format("%s-%s", var.db_identifier, count.index)
   instance_class         = var.db_instace_class
