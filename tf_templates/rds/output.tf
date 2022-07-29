c59b477c2873d76c2a6055d1df3f2add5b335e7b
Your Name <thusitha.m@informaticsint.com>
2022-07-29 15:54:44 +0530

    Testing userdata status

--- tf_templates/rds/output.tf
+++ tf_templates/rds/output.tf
@@ -1,31 +1,31 @@
-# output "db_password" {
-#   value = random_string.postgres-db-password.result
-# }
-
-# output "db_username" {
-#   value = aws_db_instance.db.username
-# }
-
-# output "rds_endpoint" {
-#   value = aws_db_instance.db.address
-# }
-
-# output "rds_port" {
-#   value = aws_db_instance.db.port
-# }
-
 output "db_password" {
-  value = "jklajdk"
+  value = random_string.postgres-db-password.result
 }
 
 output "db_username" {
-  value = "lahiie"
+  value = aws_db_instance.db.username
 }
 
 output "rds_endpoint" {
-  value = "jskkllks"
+  value = aws_db_instance.db.address
 }
 
 output "rds_port" {
-  value = "5434"
+  value = aws_db_instance.db.port
 }
+
+# output "db_password" {
+#   value = "jklajdk"
+# }
+
+# output "db_username" {
+#   value = "lahiie"
+# }
+
+# output "rds_endpoint" {
+#   value = "jskkllks"
+# }
+
+# output "rds_port" {
+#   value = "5434"
+# }
