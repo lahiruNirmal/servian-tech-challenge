53d001f9633accb949d8b8b2b7fac56ba01eee7a
Your Name <thusitha.m@informaticsint.com>
2022-07-29 09:10:52 +0530

    Testing userdata status

--- tf_templates/compute/userdata/userdata.sh
+++ tf_templates/compute/userdata/userdata.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#! /bin/bash -xe
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-sudo su
 # Download the release v.0.10.0
-mkdir /app && cd /app
+mkdir app && cd app
 yum update -y
 yum install -y wget unzip jq curl
 
