4a03c7faf6306a0434ab7b12ea8ecf64e8cc4ea2
Your Name <thusitha.m@informaticsint.com>
2022-07-29 12:01:54 +0530

    Testing userdata status

--- tf_templates/compute/userdata/userdata.sh
+++ tf_templates/compute/userdata/userdata.sh
@@ -1,12 +1,12 @@
 #! /bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-sleep 15
 # Download the release v.0.10.0
 mkdir app && cd app
 # yum install -y wget unzip jq curl
 
 # Get secrets from AWS secret manager
+export AWS_DEFAULT_REGION=region
 secret=$(aws secretsmanager get-secret-value --secret-id db_secret)
 DB_USERNAME=$(echo ${secret} | jq '.SecretString | fromjson | .db_username')
 DB_PASSWORD=$(echo ${secret} | jq '.SecretString | fromjson | .db_password')
@@ -15,6 +15,11 @@ DB_PORT=$(echo ${secret} | jq '.SecretString | fromjson | .db_port')
 LISTEN_HOST=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 echo ${secret}
+echo ${DB_USERNAME}
+echo ${DB_PASSWORD}
+echo ${DB_ENDPOINT}
+echo ${DB_PORT}
+echo ${LISTEN_HOST}
 
 # Configure conf.toml
 wget https://github.com/servian/TechChallengeApp/releases/download/v.0.10.0/TechChallengeApp_v.0.10.0_linux64.zip
@@ -28,8 +33,9 @@ sed -i 's/"DbHost" =.*/"DbHost" ="'${DB_ENDPOINT}'"/g' conf.toml
 sed -i 's/"DbPort" =.*/"DbPort" ="'${DB_PORT}'"/g' conf.toml
 sed -i 's/"ListenHost" =.*/"ListenHost" ="'${LISTEN_HOST}'"/g' conf.toml
 
+cat conf.toml
+
 # Get the number of instances in the ASG. If instances are 0, then update db operation needed.
-export AWS_DEFAULT_REGION=region
 ASG=$(aws autoscaling describe-auto-scaling-groups)
 ASG_INSTANCE_COUNT=$(echo ${ASG} | jq -c '.AutoScalingGroups[] | select(.AutoScalingGroupName | contains("test-asg")) | .Instances | length')
 ASG_INSTANCE_COUNT=$((ASG_INSTANCE_COUNT))
